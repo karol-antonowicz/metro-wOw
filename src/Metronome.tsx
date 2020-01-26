99de2b7e0084c4c6c2a55d28b7229110afdcac80
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-26 23:12:49 +0100

    managed to import audio files to typescript using es5 require(..)

--- src/Metronome.tsx
+++ src/Metronome.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Metronome.module.css';
+const click1 = require('./sounds/click1.wav')
+const click2 = require('./sounds/click2.wav')
 
 const initialBeats = {
   bpm: 120,
@@ -12,19 +14,24 @@ const Metronome = () => {
 
   const [beat, setBeat] = useState(initialBeats)
 
- const handleBpmChange = (e: any) => {
-   let bpms = e.target.value;
-   setBeat({...beat, bpm:bpms})
+  const tick = new Audio(click1)
+  const tock = new Audio(click2)
 
- }
+  const handleBpmChange = (e: any) => {
+    let bpms = e.target.value;
+    setBeat({ ...beat, bpm: bpms })
+  }
+
+
+
 
   return (
     <div className={styles.metronome}>
       <div className={styles.bpmslider}>
         <span className={styles.bpmslider_display}>Bpm {beat.bpm}</span>
-        <input type='range' min='60' max='180' value={beat.bpm} onChange={handleBpmChange} className={styles.bpmslider_range}/>
+        <input type='range' min='60' max='180' value={beat.bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
       </div>
-      <button className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
+      <button onClick={()=>{tick.play()}} className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
     </div>
   );
 }
