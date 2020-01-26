947fa4b57a70d01bff33f21af15d83978503ab75
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-26 22:50:21 +0100

    passed handleBpmChange to slider

--- src/Metronome.tsx
+++ src/Metronome.tsx
@@ -3,24 +3,28 @@ import styles from './Metronome.module.css';
 
 const initialBeats = {
   bpm: 120,
-  playing: false,
+  isPlaying: false,
   count: 0,
   beatsPerMeasure: 4,
-
 }
-
 
 const Metronome = () => {
 
   const [beat, setBeat] = useState(initialBeats)
 
+ const handleBpmChange = (e: any) => {
+   let bpms = e.target.value;
+   setBeat({...beat, bpm:bpms})
+
+ }
+
   return (
     <div className={styles.metronome}>
       <div className={styles.bpmslider}>
         <span className={styles.bpmslider_display}>Bpm {beat.bpm}</span>
-        <input type='range' min='60' max='180' value={beat.bpm} className={styles.bpmslider_range}/>
+        <input type='range' min='60' max='180' value={beat.bpm} onChange={handleBpmChange} className={styles.bpmslider_range}/>
       </div>
-      <button className={styles.metronome_btn}>{beat.playing ? 'Stop' : 'Play'}</button>
+      <button className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
     </div>
   );
 }
