0b8221a05aa4182f4a783c43b0e326f81fb410ae
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-27 01:04:39 +0100

    tick playing according to BPM, works fine but dunno how to add different sound on beat 1 as the asynchronus functions mess it all up. ffs..

--- src/Metronome.tsx
+++ src/Metronome.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Metronome.module.css';
 const click1 = require('./sounds/click1.wav')
-const click2 = require('./sounds/click2.wav')
 
 const initialBeats = {
   bpm: 120,
@@ -16,18 +15,24 @@ const Metronome = () => {
   const [timer, setTimer] = useState();
 
   const tick = new Audio(click1)
-  const tock = new Audio(click2)
 
   const handleBpmChange = (e: any) => {
-    let bpms = e.target.value;
-    setBeat({ ...beat, bpm: bpms })
+    const bpms = e.target.value;
+    if (beat.isPlaying) {
+      // if its playing then stop the old timer n start a new one.
+      clearInterval(timer);
+      setTimer(setInterval(playClick, (60 / beat.bpm) * 1000))
+      // set the new bpm and reset the beat counter
+    }
+    setBeat({ ...beat, bpm: parseInt(bpms) })
   }
 
   const playClick = () => {
-    console.log('click')
+    tick.play()
+
   }
 
-  const startStop = function() {
+  const startStop = function () {
     // if not playing - start playing in intervals acc. to BPM...;)
     if (!beat.isPlaying) {
       setTimer(setInterval(playClick, (60 / beat.bpm) * 1000))
@@ -52,7 +57,7 @@ const Metronome = () => {
         <span className={styles.bpmslider_display}>Bpm {beat.bpm}</span>
         <input type='range' min='60' max='180' value={beat.bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
       </div>
-      <button onClick={() => { startStop()}} className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
+      <button onClick={() => { startStop() }} className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
     </div>
   );
 }
