43373dd8e9433aac077faab12c16e28598a1f9fb
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-28 16:46:32 +0100

    added comments for clarity, styles,  useEffect depends on bpm, playing or playClickCallback function

--- src/Metronome.tsx
+++ src/Metronome.tsx
@@ -7,31 +7,31 @@ const Metronome = () => {
   const [playing, setPlaying] = useState(false);  // initially the metronome is not playing
   const [count, setCount] = useState(0);          // initially the count is 0. This is for the first beat to be different sound
   const [bpm, setBpm] = useState(100);            // beats per minute, the frequency of metronome
-  const timer:React.MutableRefObject<any> = useRef();                         // to store current timer id, this way it can be cleared in the useEffect hook
-  const beatsPerMeasure = 4; 
+  const timer: React.MutableRefObject<any> = useRef();                         // to store current timer id, this way it can be cleared in the useEffect hook
+  const beatsPerMeasure = 4;
 
   const tick = new Audio(click1);                 // wav file
   const tock = new Audio(click2);                 // wav file
 
-  const playClickCallback = useCallback (() => {   
-    if (count%beatsPerMeasure===0) {
+  const playClickCallback = useCallback(() => {
+    if (count % beatsPerMeasure === 0) {
       tock.play()
     } else {
-    tick.play()
+      tick.play()
     }
-    setCount(prevCount=>(prevCount+1)%beatsPerMeasure)
+    setCount(prevCount => (prevCount + 1) % beatsPerMeasure)  // update count after playing ticktock
   }, [count, tick, tock]);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (playing) {
       clearInterval(timer.current)
-      timer.current= setInterval(playClickCallback, (60/bpm)*1000)
+      timer.current = setInterval(playClickCallback, (60 / bpm) * 1000)
     } else {
       clearInterval(timer.current)
     }
-  }, [bpm, playing, playClickCallback])
+  }, [bpm, playing, playClickCallback])                 // watch out for bpm, playing, or playClickCallback function changes
 
-    // if not using callback, have to add all dependent state and props that function used to the dependency array
+  // if not using callback, have to add dependent state and props that function used to the dependency array
   // in playClick method, we should cover count, click1, and click2
   // useEffect(() => {
   //   if (playing) {  
@@ -42,7 +42,7 @@ const Metronome = () => {
   //   }
   // }, [bpm, playing, playClick, count, click1, click2])  
 
-  const startStop = () => {
+  const startStop = () => {                           // this is for starting/stopping the metronome
     if (playing) {
       setPlaying(false)
     } else {
@@ -51,7 +51,7 @@ const Metronome = () => {
     }
   }
 
-  const handleBpmChange = (e:any) => {
+  const handleBpmChange = (e: any) => {            // handler for changing bpm value via slider
     setBpm(e.target.value)
 
     if (playing) {
@@ -62,12 +62,16 @@ const Metronome = () => {
 
 
   return (
-    <div className={styles.metronome}>
-      <div className={styles.bpmslider}>
-        <span className={styles.bpmslider_display}>Bpm {bpm}</span>
-        <input type='range' min='60' max='180' value={bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
+    <div className={styles.wrapper}>
+      <div className={styles.metronome}>
+        <div className={styles.bpmslider}>
+          <span className={styles.bpmslider_display}>Bpm {bpm}</span>
+          <input type='range' min='60' max='180' value={bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
+        </div>
+        <button onClick={() => { startStop() }} className={styles.metronome_btn}>{playing ? 'Stop' : 'Play'}</button>
       </div>
-      <button onClick={() => { startStop() }} className={styles.metronome_btn}>{playing ? 'Stop' : 'Play'}</button>
+      <p className={styles.info}>Flipped <a href='https://daveceddia.com/'>Dave Ceddia's</a> Metronome<br/>
+      <a href='https://github.com/karol-antonowicz'>my github</a></p>
     </div>
   );
 }
