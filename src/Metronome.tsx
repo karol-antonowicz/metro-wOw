a99e11aa18b47e31d766485176c769a8ba6b07c2
Karol Antonowicz <karol.antonowicz@gmail.com>
2020-01-28 16:28:04 +0100

    managed to make the first beat diffferent sound using useEffect and useRef for timer

--- src/Metronome.tsx
+++ src/Metronome.tsx
@@ -1,69 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import styles from './Metronome.module.css';
-const click1 = require('./sounds/click1.wav')
-const click2 = require('./sounds/click2.wav')
-
-const initialBeats = {
-  bpm: 120,
-  isPlaying: false,
-  count: 0,
-  beatsPerMeasure: 4,
-}
+const click1 = require('./sounds/click1.wav');  // importing audio files with typescript must be done via REQUIRE (...)
+const click2 = require('./sounds/click2.wav');  // importing audio files with typescript must be done via REQUIRE (...)
 
 const Metronome = () => {
+  const [playing, setPlaying] = useState(false);  // initially the metronome is not playing
+  const [count, setCount] = useState(0);          // initially the count is 0. This is for the first beat to be different sound
+  const [bpm, setBpm] = useState(100);            // beats per minute, the frequency of metronome
+  const timer:React.MutableRefObject<any> = useRef();                         // to store current timer id, this way it can be cleared in the useEffect hook
+  const beatsPerMeasure = 4; 
 
-  const [beat, setBeat] = useState(initialBeats);
-  const [timer, setTimer] = useState();
+  const tick = new Audio(click1);                 // wav file
+  const tock = new Audio(click2);                 // wav file
 
-  const tick = new Audio(click1)
-  const tock = new Audio(click2)
-
-  const handleBpmChange = (e: any) => {
-    const bpms = e.target.value;
-    if (beat.isPlaying) {
-      // if its playing then stop the old timer n start a new one.
-      clearInterval(timer);
-      setTimer(setInterval(playClick, (60 / beat.bpm) * 1000))
-      // set the new bpm and reset the beat counter
-    }
-    setBeat({ ...beat, bpm: parseInt(bpms), count:0 })
-  }
-
-  const playClick = () => {
-    if (beat.count%beat.beatsPerMeasure==0) {
+  const playClickCallback = useCallback (() => {   
+    if (count%beatsPerMeasure===0) {
       tock.play()
     } else {
     tick.play()
     }
-    setBeat(prevBeat=> { return { ...prevBeat, count: prevBeat.count+1 }});
-  }
+    setCount(prevCount=>(prevCount+1)%beatsPerMeasure)
+  }, [count, tick, tock]);
 
-  const startStop = function () {
-    // if not playing - start playing in intervals acc. to BPM...;)
-    if (!beat.isPlaying) {
-      setTimer(setInterval(playClick, (60 / beat.bpm) * 1000))
-      setBeat({
-        ...beat,
-        count: 0,
-        isPlaying: true
-      })
+  useEffect(()=>{
+    if (playing) {
+      clearInterval(timer.current)
+      timer.current= setInterval(playClickCallback, (60/bpm)*1000)
     } else {
-      clearInterval(timer);
-      setBeat({
-        ...beat,
-        isPlaying: false
-      })
+      clearInterval(timer.current)
+    }
+  }, [bpm, playing, playClickCallback])
+
+    // if not using callback, have to add all dependent state and props that function used to the dependency array
+  // in playClick method, we should cover count, click1, and click2
+  // useEffect(() => {
+  //   if (playing) {  
+  //     clearInterval(timer.current)
+  //     timer.current = setInterval(playClick, (60/bpm)*1000)
+  //   } else {
+  //     clearInterval(timer.current)
+  //   }
+  // }, [bpm, playing, playClick, count, click1, click2])  
+
+  const startStop = () => {
+    if (playing) {
+      setPlaying(false)
+    } else {
+      setCount(0)
+      setPlaying(true)
     }
   }
+
+  const handleBpmChange = (e:any) => {
+    setBpm(e.target.value)
+
+    if (playing) {
+      setCount(0)
+    }
+  }
+
 
 
   return (
     <div className={styles.metronome}>
       <div className={styles.bpmslider}>
-        <span className={styles.bpmslider_display}>Bpm {beat.bpm}</span>
-        <input type='range' min='60' max='180' value={beat.bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
+        <span className={styles.bpmslider_display}>Bpm {bpm}</span>
+        <input type='range' min='60' max='180' value={bpm} onChange={handleBpmChange} className={styles.bpmslider_range} />
       </div>
-      <button onClick={() => { startStop() }} className={styles.metronome_btn}>{beat.isPlaying ? 'Stop' : 'Play'}</button>
+      <button onClick={() => { startStop() }} className={styles.metronome_btn}>{playing ? 'Stop' : 'Play'}</button>
     </div>
   );
 }
